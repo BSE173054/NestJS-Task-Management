6bbbfad3a6c19c42267aeacaa6b4706056310bde
Jerrold Sigamony <jerald.sigamony@gmail.com>
2022-01-24 13:25:18 +0500

    Update and Filter

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-fikter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,6 +11,24 @@ export class TasksService {
   //Get All Tasks
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterTaskdto: GetTasksFilterDto): Task[] {
+    const { search, status } = filterTaskdto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+    }
+    return tasks;
   }
 
   //Get Task By ID
@@ -33,5 +52,12 @@ export class TasksService {
   //Delete Task
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  //Update Task Status
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 }
